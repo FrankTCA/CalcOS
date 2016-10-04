c3523f7e6722e1656ad798456b716cc8c60d01e5
FrankTCA <frank@steamedveggie.com>
2016-10-04 08:52:21 -0500

    Kernal.cpp Add Some Methods 1

--- central_source/kernal.cpp
+++ central_source/kernal.cpp
@@ -34,4 +34,50 @@ enum vga_color {
   VGA_COLOR_WHITE = 15
 }
 
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+  return fg | bg << 4;
+}
+
+static inline unit16_t vga_entry(unsigned char uc, uint8_t color) {
+  return (uint16_t) uc | (unit16_t) color << 8;
+}
+
+size_t strlen(const char* str) {
+  size_t len = 0;
+  while (str[len]) {
+    len++
+  }
+  return len;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+size_t terminal_row;
+size_t terminal_column;
+unit8_t terminal_color;
+uint16_t* terminal_buffer;
+
+void terminal_initialize(void) {
+  terminal_row = 0;
+  terminal_column = 0;
+  terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GRAY, VGA_COLOR_BLACK);
+  terminal_buffer = (uint16_t*) 0xB8000;
+  for (size_t y = 0; y < VGA_HEIGHT; y++) {
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+      const size_t index = y * VGA_WIDTH + x;
+      terminal_buffer[index] = vga_entry(' ', terminal_color);
+    }
+  }
+}
+
+void terminal_setcolor(unit8_t color) {
+  terminal_color = color;
+}
+
+void putentryat(char c, uint8_t color, size_t x, size_t y) {
+  const size_t index = y * VGA_WIDTH + x;
+  terminal_buffer[index];
+}
+
 
